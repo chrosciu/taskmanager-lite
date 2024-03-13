bf3ea3b9b0c860ddc97fa7e67033c31227e19237
Marcin Chrost <mchrost82@gmail.com>
2024-03-13 16:39:29 +0100

    Communication between team and user domains done with RabbitMQ

---
+++ user-infrastructure/src/main/java/eu/chrost/taskmanager/user/RabbitUserEventListener.java
@@ -0,0 +1,24 @@
+package eu.chrost.taskmanager.user;
+
+import eu.chrost.taskmanager.commons.event.TeamMembersEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RabbitListener(queues = "taskmanager")
+@RequiredArgsConstructor
+@Slf4j
+class RabbitUserEventListener {
+    private final UserFacade userFacade;
+
+    @RabbitHandler
+    @Transactional
+    public void receive(TeamMembersEvent teamMembersEvent) {
+        log.info("{}", teamMembersEvent);
+        userFacade.handle(teamMembersEvent);
+    }
+}
