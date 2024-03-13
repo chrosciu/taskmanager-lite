d581fde7a8890d864d1659b89c06aab439437cbd
Marcin Chrost <mchrost82@gmail.com>
2024-03-13 10:03:45 +0100

    Spring event publisher moved to team package (common part of infrastructure makes no sense here)

--- infrastructure/src/main/java/eu/chrost/taskmanager/team/TeamConfiguration.java
+++ infrastructure/src/main/java/eu/chrost/taskmanager/team/TeamConfiguration.java
@@ -1,6 +1,5 @@
 package eu.chrost.taskmanager.team;
 
-import eu.chrost.taskmanager.SpringEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +8,7 @@ class TeamConfiguration {
     @Bean
     public TeamFacade teamFacade(TeamRepository teamRepository,
                                  TeamQueryRepository teamQueryRepository,
-                                 SpringEventPublisher springEventPublisher) {
-        return new TeamFacade(teamRepository, teamQueryRepository, springEventPublisher);
+                                 SpringTeamEventPublisher springTeamEventPublisher) {
+        return new TeamFacade(teamRepository, teamQueryRepository, springTeamEventPublisher);
     }
 }
