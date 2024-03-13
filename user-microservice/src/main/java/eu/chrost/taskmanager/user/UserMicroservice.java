cda7d611aaba7436b802a72c5c38cd50cd0cc296
Marcin Chrost <mchrost82@gmail.com>
2024-03-13 20:01:21 +0100

    Monolith application splitted into two independent microservices; updated HTTP requests file

--- user-microservice/src/main/java/eu/chrost/taskmanager/user/UserMicroservice.java
+++ user-microservice/src/main/java/eu/chrost/taskmanager/user/UserMicroservice.java
@@ -1,11 +1,11 @@
-package eu.chrost.taskmanager;
+package eu.chrost.taskmanager.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskManagerApplication {
+class UserMicroservice {
     public static void main(String[] args) {
-        SpringApplication.run(TaskManagerApplication.class, args);
+        SpringApplication.run(UserMicroservice.class, args);
     }
 }
